eca0e3bf277b74318aa2bf948a5d5f6753d63922
Stélio Moiane <steliomo@gmail.com>
2014-11-28 18:39:22 +0200

    O inicio do mozview frameworks

---
+++ src/main/java/mz/co/mozview/frameworks/core/App.java
@@ -0,0 +1,13 @@
+package mz.co.mozview.frameworks.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
