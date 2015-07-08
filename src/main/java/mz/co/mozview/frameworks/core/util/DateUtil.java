032463841b6fa4054ec84b6c87d43b85283c425d
Stélio Moiane <steliomo@gmail.com>
2015-07-09 00:20:13 +0200

    Utilitário para datas

---
+++ src/main/java/mz/co/mozview/frameworks/core/util/DateUtil.java
@@ -0,0 +1,89 @@
+/*
+ * MozView Technologies, Lda. 2010 - 2015
+ */
+package mz.co.mozview.frameworks.core.util;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+/**
+ * @author Stélio Moiane
+ *
+ */
+public class DateUtil {
+
+	public static Calendar getStartOfDay(final Calendar value) {
+
+		if (value == null) {
+			return null;
+		}
+
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.set(value.get(Calendar.YEAR), value.get(Calendar.MONTH), value.get(Calendar.DATE), 0, 0, 0);
+
+		return calendar;
+	}
+
+	public static Calendar getEndOfDay(final Calendar value) {
+		if (value == null) {
+			return null;
+		}
+
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.set(value.get(Calendar.YEAR), value.get(Calendar.MONTH), value.get(Calendar.DATE), 23, 59, 59);
+
+		return calendar;
+	}
+
+	public static Calendar toCalendar(final LocalDate value) {
+
+		if (value == null) {
+			return null;
+		}
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(Date.valueOf(value));
+
+		return calendar;
+	}
+
+	public static String toString(final LocalDate value) {
+
+		if (value == null) {
+			return null;
+		}
+
+		return value.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+	}
+
+	public static String toString(final LocalDate value, final String pattern) {
+
+		if (value == null) {
+			return null;
+		}
+
+		return value.format(DateTimeFormatter.ofPattern(pattern));
+	}
+
+	public static LocalDate toLocalDate(final String value) {
+
+		if (value == null) {
+			return null;
+		}
+
+		return LocalDate.parse(value, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+	}
+
+	public static LocalDate toLocalDate(final String value, final String pattern) {
+
+		if (value == null) {
+			return null;
+		}
+
+		return LocalDate.parse(value, DateTimeFormatter.ofPattern(pattern));
+	}
+}
