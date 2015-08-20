faf7aca3a4b32419d75ec47397913bec7fe94ed1
Stelio Moiane <steliomo@gmail.com>
2015-08-20 17:46:47 +0200

    Update SMS

--- src/main/java/mz/co/mozview/frameworks/core/sms/SmsSenderServiceImp.java
+++ src/main/java/mz/co/mozview/frameworks/core/sms/SmsSenderServiceImp.java
@@ -26,23 +26,23 @@ public class SmsSenderServiceImp implements SmsSenderService {
 
 	public static final String ACCOUNT_SID = "AC2d819526db958dfd7174d6df54cdfa67";
 	public static final String AUTH_TOKEN = "5f33f14b41bd6d2721a5760ff7a19de2";
-	public static final String FROM_NUMBER = "+18025461002";
+	public static final String FROM_NUMBER = "+12056078353";
 
 	@Override
 	public String send(final String toNumber, final String message) throws BusinessException {
 
-		TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+		final TwilioRestClient client = new TwilioRestClient(SmsSenderServiceImp.ACCOUNT_SID, SmsSenderServiceImp.AUTH_TOKEN);
 
-		List<NameValuePair> params = new ArrayList<>();
+		final List<NameValuePair> params = new ArrayList<>();
 		params.add(new BasicNameValuePair("Body", message));
 		params.add(new BasicNameValuePair("To", toNumber));
-		params.add(new BasicNameValuePair("From", FROM_NUMBER));
+		params.add(new BasicNameValuePair("From", SmsSenderServiceImp.FROM_NUMBER));
 
-		MessageFactory messageFactory = client.getAccount().getMessageFactory();
+		final MessageFactory messageFactory = client.getAccount().getMessageFactory();
 		Message msg = null;
 		try {
 			msg = messageFactory.create(params);
-		} catch (TwilioRestException e) {
+		} catch (final TwilioRestException e) {
 			e.printStackTrace();
 		}
 
