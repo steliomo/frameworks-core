8416d9e0b100c236ffd742d7ae2303821630680e
Stélio Moiane <steliomo@gmail.com>
2015-10-26 23:43:42 +0200

    ajustes no ficheiro de mocks

--- src/main/java/mz/co/mozview/frameworks/core/fixtureFactory/util/MockUtil.java
+++ src/main/java/mz/co/mozview/frameworks/core/fixtureFactory/util/MockUtil.java
@@ -3,6 +3,8 @@
  */
 package mz.co.mozview.frameworks.core.fixtureFactory.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.Advised;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -10,28 +12,35 @@ import org.springframework.test.util.ReflectionTestUtils;
 /**
  * @author Stélio Moiane
  *
- *         Este utilitário trata objectos mocéveis
+ *         Este utilitário trata objectos mockáveis
  */
 public final class MockUtil {
 
-	public static <T> void setFieldOnRefletion(final T mainObject, final String mockedObjectName, final T mockeObject)
-			throws Exception {
+	private static final Logger logger = LoggerFactory.getLogger(MockUtil.class);
+
+	public static <T> void mockField(final T mainObject, final String mockedObjectName, final T mockeObject) {
 
 		ReflectionTestUtils.setField(MockUtil.unwrapProxy(mainObject), mockedObjectName, mockeObject);
 	}
 
-	private static Object unwrapProxy(Object bean) throws Exception {
+	private static Object unwrapProxy(Object bean) {
 		/*
 		 * If the given object is a proxy, set the return value as the object
 		 * being proxied, otherwise return the given object.
 		 */
-		if (AopUtils.isAopProxy(bean) && bean instanceof Advised) {
+		if (AopUtils.isAopProxy(bean) && (bean instanceof Advised)) {
+			logger.info("Is about to mock");
 
 			final Advised advised = (Advised) bean;
 
-			bean = advised.getTargetSource().getTarget();
+			try {
+				bean = advised.getTargetSource().getTarget();
+				logger.info("Mock was done.....");
+			} catch (Exception e) {
+				logger.error("Problems trying to mock....");
+				e.printStackTrace();
+			}
 		}
-
 		return bean;
 	}
 }
